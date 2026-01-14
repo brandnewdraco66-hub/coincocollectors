91ea9f2689ba9023c508c567ed58494958f0d118
brandnewdraco66-hub <brandnewdraco66@gmail.com>
2026-01-14 04:52:05 +0100

    Fix build & router

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Coin-Collectors-P2E/',  // ← Ändra till ditt EXAKTA repo-namn! (stort/litet känsligt, med slashar)
+  base: '/coincocollectors/',
 })
